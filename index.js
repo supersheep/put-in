e130dc8f03876b44986132a2f3ce6509cc4f5632
supersheep <supersheepxjy@gmail.com>
2013-10-21 21:27:00 +0800

    init repo

---
+++ index.js
@@ -0,0 +1,29 @@
+var child_process = require('child_process');
+var fs_sync = require('fs-sync');
+var path = require('path');
+
+var Putin =  function(cwd){
+
+    this.cwd = cwd || process.cwd();
+    return this;
+}
+
+Putin.prototype.install = function(module,callback){
+    var cwd = this.cwd
+    var file = path.resolve(cwd,"package.json");
+
+    fs_sync.write(file, "{}");
+
+    var install = child_process.spawn('npm',['install',module],{
+        cwd:cwd,
+        stdio:"inherit"
+    });
+
+
+
+    install.on("exit",callback);
+}
+
+module.exports = function(cwd){
+    return new Putin(cwd);
+}
